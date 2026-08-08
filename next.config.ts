da5f299f46c5f28e992a14c0bf5623ead4b8e966
Ajay K M <katto.qt@gmail.com>
2026-08-08 14:23:09 +0530

    fix: sharp dependency issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["sharp"],
 };
 
 export default withPayload(nextConfig);
